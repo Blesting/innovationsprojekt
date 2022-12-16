d45578f6274c5871a1db2a9a4fa87b69c466fc5f
ZerusTS <73193664+ZerusTS@users.noreply.github.com>
2022-12-16 19:54:34 +0100

    xml parser

--- src/main/src/ApiInteg.java
+++ src/main/src/ApiInteg.java
@@ -5,6 +5,15 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.net.http.HttpRequest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.nio.Buffer;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.ByteArrayInputStream;
 
 public class ApiInteg{
     public static void getTrips(int originID, int destID, String date, String time) {
@@ -29,10 +38,35 @@ public class ApiInteg{
                 in.close();
             }
             connec.disconnect();
+            String q = content.toString();
             System.out.println(content);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    /*public static void xmlParser(String xmlFile) {
+        try {
+            // Create a DocumentBuilder
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
 
+            // Parse the XML string and get the Document object
+            String xmlString = xmlFile;
+            Document doc = builder.parse(new ByteArrayInputStream(xmlString.getBytes()));
+
+            // Get the root element
+            Node root = doc.getDocumentElement();
+
+            // Get all child nodes of the root element
+            NodeList nodes = root.getChildNodes();
+
+            // Iterate through the nodes and print their names
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                System.out.println(node.getNodeName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
 }
