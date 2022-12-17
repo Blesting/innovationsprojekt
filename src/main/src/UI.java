1762b8bc40cc1d210fa78d05d44b0c4f8c6e750d
Blesting <christiannesting@gmail.com>
2022-12-17 03:05:27 +0100

    implemented valid trip page with dummy data

--- src/main/src/UI.java
+++ src/main/src/UI.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UI{
 
@@ -10,27 +14,27 @@ public class UI{
 	private static int height = 700;
 
 	public static void buildHomepage(){
-		JFrame loginPage = new JFrame("homepage");
+		JFrame homePage = new JFrame("homepage");
 
-		//Login label
-		JLabel loginLabel = new JLabel("search trip");
-		loginLabel.setBounds(20,50,400,100);
-		loginLabel.setFont(new Font("Serif",Font.PLAIN,60));
-		loginPage.add(loginLabel);
+		//Search trip label
+		JLabel searchTripLabel = new JLabel("search trip");
+		searchTripLabel.setBounds(20,20,400,100);
+		searchTripLabel.setFont(new Font("Serif",Font.PLAIN,60));
+		homePage.add(searchTripLabel);
 
 		//fra field
 		JTextField fromField = new JTextField();
 		JLabel fromLabel = new JLabel("fra:");
 		fromLabel.setBounds(20,160, 80,30);
 		fromField.setBounds(100,160,100,30);
-		loginPage.add(fromField);  loginPage.add(fromLabel);
+		homePage.add(fromField);  homePage.add(fromLabel);
 
 		//til field
 		JTextField toField = new JTextField();
 		JLabel toLabel = new JLabel("til:");
 		toLabel.setBounds(20,200, 80,30);
 		toField.setBounds(100,200,100,30);
-		loginPage.add(toField);  loginPage.add(toLabel);
+		homePage.add(toField);  homePage.add(toLabel);
 
 		//tid fields
 		JTextField fromtimeField = new JTextField();
@@ -39,12 +43,12 @@ public class UI{
 		timeLabel.setBounds(20,240, 80,30);
 		fromtimeField.setBounds(100,240,100,30);
 		totimeField.setBounds(200,240,100,30);
-		loginPage.add(fromtimeField); loginPage.add(totimeField);  loginPage.add(timeLabel);
+		homePage.add(fromtimeField); homePage.add(totimeField);  homePage.add(timeLabel);
 
 
 		JLabel wrongInputLabel = new JLabel();
 		wrongInputLabel.setBounds(100,320,300,30);
-		loginPage.add(wrongInputLabel);
+		homePage.add(wrongInputLabel);
 
 		//search button
 		JButton searchButton = new JButton("Search");
@@ -52,15 +56,89 @@ public class UI{
 		searchButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				String fromTime = fromtimeField.getText();
+				String toTime = totimeField.getText();
 
+				int[] stationIds = main.getStationIds(fromField.getText(), toField.getText());
+				int fromStationId = stationIds[0];
+				int toStationId = stationIds[1];
+
+				if(fromStationId == toStationId)
+					wrongInputLabel.setText("to and from station cannot be the same");
+				else if(fromStationId == 0 || toStationId == 0)
+					wrongInputLabel.setText("at least one station doesn't exist");
+				else if(fromTime.isBlank() || toTime.isBlank())
+					wrongInputLabel.setText("you must enter a time"); // TODO check om tid er korrekt format
+				else
+					wrongInputLabel.setText(":)");
+
+				if(wrongInputLabel.getText().equals(":)") && fromStationId == 8600512 && toStationId == 8600626){
+					homePage.dispose();
+					buildMainPage(fromStationId, toStationId, fromTime, toTime);
+				}
 			}
 		});
-		loginPage.add(searchButton);
+		homePage.add(searchButton);
 
 
-		loginPage.dispatchEvent(new WindowEvent(loginPage, WindowEvent.WINDOW_CLOSING));
-		loginPage.setSize(width, height);
-		loginPage.setLayout(null);
-		loginPage.setVisible(true);
+		homePage.dispatchEvent(new WindowEvent(homePage, WindowEvent.WINDOW_CLOSING));
+		homePage.setSize(width, height);
+		homePage.setLayout(null);
+		homePage.setVisible(true);
+	}
+
+	public static void buildMainPage(int fromStation, int toStation, String fromTime, String toTime){
+		JFrame mainPage = new JFrame("Main page");
+
+		//valid trips label
+		JLabel validTripsLabel = new JLabel("Valid trips");
+		validTripsLabel.setBounds(20,20,400,100);
+		validTripsLabel.setFont(new Font("Serif",Font.PLAIN,60));
+
+		//Valid trips table
+		ArrayList<String> validTrips = main.getValidTrips(fromStation, toStation, fromTime, toTime);
+
+		String[] fields = {"departure", "arrival", "Price", "ESB", "Buy ticket"};
+		Object[][] data = new Object[(int) validTrips.size()][5];
+
+		//Fills data table with items
+		int index = 0;
+		for (String trip : validTrips){
+			String[] tripArray = trip.split(",");
+			String seatBought = "";
+			ArrayList<String> seatData = main.getDummyData(tripArray[0]);
+			if(seatData.size() == 0){
+				seatBought = "No data";
+			}
+			else{
+				double average = 0.0;
+				for (String seatDatum : seatData) {
+					String[] seatDatumArr = seatDatum.split(",");
+					average = average + Double.valueOf(seatDatumArr[3])/Double.valueOf(seatDatumArr[2]);
+				}
+				average = average / seatData.size() * 100;
+				seatBought = String.valueOf(average);
+			}
+			data[index][0] = tripArray[1];
+			data[index][1] = tripArray[2];
+			data[index][2] = "$$$";
+			data[index][3] = seatBought;
+			data[index][4] = "Button";
+			index ++;
+		}
+		JTable table = new JTable(data, fields){
+			//No columns should be editable
+			public boolean isCellEditable(int row, int column){ return column == 0; }
+		};
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+
+		mainPage.add(validTripsLabel, BorderLayout.NORTH);
+		mainPage.add(scrollPane, BorderLayout.CENTER);
+
+
+		mainPage.dispatchEvent(new WindowEvent(mainPage, WindowEvent.WINDOW_CLOSING));
+		mainPage.setSize(width, height);
+		mainPage.setVisible(true);
 	}
 }
--- src/main/src/main.java
+++ src/main/src/main.java
@@ -1,59 +1,86 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class main{
 
-	public static void main(String[] arc){
-		UI.buildHomepage();
-		ArrayList<Station> stationer = new ArrayList();
-		ArrayList<Trip> trips = new ArrayList<>();
+	private static ArrayList<Station> stations = new ArrayList();
+	private static ArrayList<Trip> trips = new ArrayList<>();
 
+	public static void main(String[] arc){
+
+		loadData();
+
+		UI.buildHomepage();
+
+
+		/*
+		int[] stationIds = getStationIds("Odense St.", "København H");
+		getValidTrips(stationIds[0], stationIds[1], "17:00:00", "20:00:00").forEach(System.out::println);
+		*/
+
+		//trips.forEach(trip -> System.out.println(trip.getTripHeadsign()));
+		//ApiInteg.getTrips(8600646, 8600798, "22/12/2022", "17:55");
+		//System.out.println(trips.size());
+
+	}
+
+	public static ArrayList<Station> getStations() {
+		return stations;
+	}
+
+	public static ArrayList<Trip> getTrips() {
+		return trips;
+	}
+
+	public static void loadData(){
 		try {
 			HashMap<Integer, String[]> tripTemp = new HashMap();
 			HashMap<Integer, ArrayList<String>> timesTemp = new HashMap();
 
 			// Loading products
-			String fileString = Files.readString(Path.of("../data/RejseplanenStoppesteder.txt"));
+			String fileString = Files.readString(Path.of("src/main/data/RejseplanenStoppesteder.txt"));
 			fileString
-				.lines()
-				.skip(1)
-				.map(line -> line.split(";"))
-				.filter(lineArray -> lineArray[0].length() == 7)
-				//.peek(System.out::println)
-				.forEach(lineArray -> {
-					Location location = new Location(Integer.valueOf(lineArray[2]), Integer.valueOf(lineArray[3]));
-					Station station = new Station(Integer.valueOf(lineArray[0]), lineArray[1], location);
-					stationer.add(station);
-				});
+					.lines()
+					.skip(1)
+					.map(line -> line.split(";"))
+					.filter(lineArray -> lineArray[0].length() == 7)
+					//.peek(System.out::println)
+					.forEach(lineArray -> {
+						Location location = new Location(Integer.valueOf(lineArray[2]), Integer.valueOf(lineArray[3]));
+						Station station = new Station(Integer.valueOf(lineArray[0]), lineArray[1].substring(1,lineArray[1].length() - 1), location);
+						stations.add(station);
+					});
 
-			fileString = Files.readString(Path.of("../data/trips.txt"));
+			fileString = Files.readString(Path.of("src/main/data/trips.txt"));
 			fileString
-				.lines()
-				.skip(1)
-				.map(line -> line.split(","))
-				//.peek(System.out::println)
-				.forEach(lineArray -> tripTemp.put(Integer.valueOf(lineArray[2]), lineArray));
+					.lines()
+					.skip(1)
+					.map(line -> line.split(","))
+					//.peek(System.out::println)
+					.forEach(lineArray -> tripTemp.put(Integer.valueOf(lineArray[2]), lineArray));
 
-			fileString = Files.readString(Path.of("../data/stop_times.txt"));
+			fileString = Files.readString(Path.of("src/main/data/stop_times.txt"));
 			fileString
-				.lines()
-				.skip(1)
-				.map(line -> line.split(","))
-				//.peek(System.out::println)
-				.forEach(lineArray -> {
-					Integer tripId = Integer.valueOf(lineArray[0]);
-					if(!timesTemp.containsKey(tripId)){
-						ArrayList<String> time = new ArrayList();
-						time.add(lineArray[1]);
-						time.add(lineArray[2]);
-						timesTemp.put(tripId, time);
-					}
-					else{
-						timesTemp.get(tripId).add(lineArray[1]);
-						timesTemp.get(tripId).add(lineArray[2]);
-					}
-				});
+					.lines()
+					.skip(1)
+					.map(line -> line.split(","))
+					//.peek(System.out::println)
+					.forEach(lineArray -> {
+						Integer tripId = Integer.valueOf(lineArray[0]);
+						if(!timesTemp.containsKey(tripId)){
+							ArrayList<String> time = new ArrayList();
+							time.add(lineArray[1]);
+							time.add(lineArray[2]);
+							timesTemp.put(tripId, time);
+						}
+						else{
+							timesTemp.get(tripId).add(lineArray[1]);
+							timesTemp.get(tripId).add(lineArray[2]);
+						}
+					});
+
 
 			timesTemp.forEach((key, value) -> {
 				if(tripTemp.containsKey(key)){
@@ -64,20 +91,139 @@ public class main{
 				}
 			});
 
-			//timesTemp.forEach((key,value) -> System.out.println(key));
+		} catch (Exception e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
 
-			//System.out.println(stationer.size());
+	public static ArrayList<String> getValidTrips(int fromStationInt, int toStationInt, String fromTime, String toTime){
+		ArrayList<String> validTrips = new ArrayList<>();
 
+		try {
+
+			String fromStation = "00000" + fromStationInt;
+			String toStation = "00000" + toStationInt;
+
+			String[] fromTimeArray = fromTime.split(":");
+			String[] toTimeArray = toTime.split(":");
+
+
+			HashMap<String, ArrayList<String>> testMap = new HashMap<>();
+
+			String fileString = Files.readString(Path.of("src/main/data/stop_times.txt"));
+			fileString
+					.lines()
+					.skip(1)
+					.map(line -> line.split(","))
+					.filter(lineArray -> lineArray[3].equals(fromStation) || lineArray[3].equals(toStation))
+					//.peek(System.out::println)
+					.forEach(lineArray -> {
+						if(lineArray[3].equals(fromStation)){
+							if(!testMap.containsKey(lineArray[0])){
+								ArrayList<String> stop = new ArrayList();
+								stop.add(lineArray[3] + "," + lineArray[2]);
+								testMap.put(lineArray[0], stop);
+							}
+							else{
+								testMap.get(lineArray[0]).add(lineArray[3] + "," + lineArray[2]);
+							}
+						}
+						else{
+							String[] arrivalTimeArray = lineArray[1].split(":");
+
+							Boolean later = false;
+							if(Integer.valueOf(arrivalTimeArray[0]) >= Integer.valueOf(fromTimeArray[0])){
+								if(Integer.valueOf(arrivalTimeArray[1]) >= Integer.valueOf(fromTimeArray[1])){
+									if(Integer.valueOf(arrivalTimeArray[2]) >= Integer.valueOf(fromTimeArray[2])){
+										later = true;
+									}
+								}
+							}
+							Boolean earlier = false;
+							if(Integer.valueOf(arrivalTimeArray[0]) < Integer.valueOf(toTimeArray[0])){
+								earlier = true;
+							}
+							else if(Integer.valueOf(arrivalTimeArray[0]) == Integer.valueOf(toTimeArray[0])){
+								if(Integer.valueOf(arrivalTimeArray[1]) < Integer.valueOf(toTimeArray[1])){
+									earlier = true;
+								}
+								else if(Integer.valueOf(arrivalTimeArray[1]) == Integer.valueOf(toTimeArray[1])){
+									if(Integer.valueOf(arrivalTimeArray[1]) <= Integer.valueOf(toTimeArray[1])) {
+										earlier = true;
+									}
+								}
+							}
+
+							if(later && earlier){
+								if(!testMap.containsKey(lineArray[0])){
+									ArrayList<String> stop = new ArrayList();
+									stop.add(lineArray[3] + "," + lineArray[1]);
+									testMap.put(lineArray[0], stop);
+								}
+								else{
+									testMap.get(lineArray[0]).add(lineArray[3] + "," + lineArray[1]);
+								}
+							}
+						}
+
+
+					});
+			/*
+			testMap.entrySet().stream()
+					.filter(x -> x.getValue().size() == 2 && x.getValue().get(0).equals(fromStation))
+					.forEach(x -> {
+						System.out.print(x.getKey() + ": ");
+						x.getValue().forEach(value -> System.out.print(value + " "));
+						System.out.println();
+					});
+			*/
+
+			testMap.entrySet().stream()
+					.filter(x -> x.getValue().size() == 2 && x.getValue().get(0).split(",")[0].equals(fromStation))
+					.forEach(x -> validTrips.add(x.getKey() + "," + x.getValue().get(0).split(",")[1] + "," + x.getValue().get(1).split(",")[1]));
 
 		} catch (Exception e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
+		return validTrips;
+	}
 
-		//trips.forEach(trip -> System.out.println(trip.getTripHeadsign()));
-		ApiInteg.getTrips(8600646, 8600798, "22/12/2022", "17:55");
-		//System.out.println(trips.size());
+	public static int[] getStationIds(String fromStationName, String toStationName){
+		int[] stationIds = {0,0};
 
+		for (Station station : stations) {
+			if(station.getName().equals(fromStationName))
+				stationIds[0] = station.getId();
+			else if(station.getName().equals(toStationName))
+				stationIds[1] = station.getId();
+		}
+
+
+		return stationIds;
+	}
+
+	public static ArrayList<String> getDummyData(String tripId){
+		ArrayList<String> dummyData = new ArrayList<>();
+
+		try{
+			String fileString = Files.readString(Path.of("src/main/data/dummyData.txt"));
+			fileString
+					.lines()
+					.skip(1)
+					.map(line -> line.split(","))
+					.filter(lineArray -> lineArray[0].equals(tripId))
+					//.peek(System.out::println)
+					.forEach(lineArray -> {
+						dummyData.add(lineArray[0] + "," + lineArray[1] + "," + lineArray[2] + "," + lineArray[3]);
+						});
+		}catch (Exception e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+		return dummyData;
 	}
 
 
